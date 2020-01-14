7f0cc3ecc4976ab8078e611136ff2a17a5a054fe
sonderangebot10 <37372712+sonderangebot10@users.noreply.github.com>
2020-01-14 16:47:15 +0100

    minor improvements

--- tic-tac-toe-react/src/components_/Board.js
+++ tic-tac-toe-react/src/components_/Board.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
 
 class Board extends Component {
   constructor(props){
@@ -10,17 +11,22 @@ class Board extends Component {
         playing: false,
         board: [0,0,0,0,0,0,0,0,0],
         turn: Math.floor((Math.random() * 2) + 1),
-        text_field: ''
+        text_field: '',
+        speed: 50,
+        numberG: 100,
+        wins: [0, 0]
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handlePlay = this.handlePlay.bind(this);
     this.handleReset = this.handleReset.bind(this);
+    this.reset = this.reset.bind(this);
     this.send_request = this.send_request.bind(this);
+    this.play = this.play.bind(this);
   }
 
   componentDidMount() {
-    this.setState({text_field: this.state.text_field + 'Enter ports of player one and two and hit play'});
+    this.setState({text_field: this.state.text_field + 'Enter ports of player one and two and hit play' + '\n'});
   }
 
   handleChange(e) {
@@ -30,30 +36,46 @@ class Board extends Component {
 
   handlePlay(e) {
     e.preventDefault();
+    this.setState({ playing: true });
+    this.numberG_ = this.state.numberG; //saving for reset
+    this.play();
+  }
+
+    play() {
     const { port1, port2 } = this.state;
     let participants = [port1, port2];
-    this.setState({ playing: true });
+      this.interval = setInterval(async () => {
 
-    this.interval = setInterval(async () => {
         //send request and await response
-        await this.send_request(this.state.turn, this.state.board, participants[this.state.turn - 1], this);
+        await this.send_request(this.state.turn, this.state.board, participants[this.state.turn - 1]);
 
-        //check if anyone has one
+        //check if anyone has won
         if(hasWon(this.state.board, this.state.turn)) {
+            this.setState({numberG: this.state.numberG - 1})
+            this.state.wins[this.state.turn - 1] = this.state.wins[this.state.turn - 1] + 1;
             this.setState({text_field: this.state.text_field + 'Player ' + this.state.turn + ' has won!' + '\n'})
-            clearInterval(this.interval);
+            this.reset();
+
+            if(this.state.numberG > 0) {
+              this.play();
+            }
         }
 
         //if cells are full
         else if(!this.state.board.includes(0)) {
+            this.setState({numberG: this.state.numberG - 1})
             this.setState({text_field: this.state.text_field + 'Noone won.' + '\n'});
-            clearInterval(this.interval);
+            this.reset();
+
+            if(this.state.numberG > 0) {
+              this.play();
+            }
         }
 
         (this.state.turn === 1) ? this.setState({turn: 2}) : this.setState({turn: 1});
-    }, 2000);
 
-  }
+    }, this.state.speed);
+    }
 
    async send_request(turn, board, port) {
     const requestOptions = {
@@ -77,15 +99,12 @@ class Board extends Component {
             }
 
             this.setState({text_field: this.state.text_field + 'Player ' + turn + ' placed cell on ' + json.cell + '\n'})
-            board[json.cell] = turn;
-            // console.log(board[0] + ' ' + board[1] + ' ' + board[2]);
-            // console.log(board[3] + ' ' + board[4] + ' ' + board[5]);
-            // console.log(board[6] + ' ' + board[7] + ' ' + board[8]);
-            
+            board[json.cell] = turn;            
         })
         .catch(error => console.log(error));
 }
 
+  //resets state of everything
   handleReset(e) {
     e.preventDefault();
     this.setState({text_field: this.state.text_field + 'Reset' + '\n'})
@@ -94,11 +113,18 @@ class Board extends Component {
     // [0, 1, 2]
     // [3, 4, 5]
     // [6, 7, 8]
-    this.setState({turn: Math.floor((Math.random() * 2) + 1), playing: false});
+    this.setState({turn: Math.floor((Math.random() * 2) + 1), playing: false, wins: [0, 0], numberG: this.numberG_, text_field: ''});
+  }
+
+  //resets the only the game state
+  reset() {
+    clearInterval(this.interval);
+    this.setState({board: [0, 0, 0, 0, 0, 0, 0, 0, 0]});
+    this.setState({turn: Math.floor((Math.random() * 2) + 1)});
   }
 
   render() {
-    const { port1, port2, playing } = this.state;
+    const { port1, port2, playing, speed, numberG } = this.state;
     return (
         <div>
           Turn {this.state.turn}
@@ -112,20 +138,32 @@ class Board extends Component {
           <form name="form" onSubmit={this.handleSubmit}>
                     <div className='form-group'>
                         <label htmlFor="username">Port 1 (X) </label>
-                        <input type="text" className="form-control" name="port1" value={port1} onChange={this.handleChange} />
+                        <input type="text" className="form-control" name="port1" disabled={playing} value={port1} onChange={this.handleChange} />
                     </div>
                     <div className='form-group'>
                         <label htmlFor="password">Port 2 (O) </label>
-                        <input type="text" className="form-control" name="port2" value={port2} onChange={this.handleChange} />
+                        <input type="text" className="form-control" name="port2" disabled={playing} value={port2} onChange={this.handleChange} />
                     </div>
-                    <button variant="contained" color="primary" disabled={playing} onClick={this.handlePlay} className="btn btn-primary">Play</button>
-                    <button variant="contained" color="primary" onClick={this.handleReset} className="btn btn-primary">Reset</button>
+                    <div className='form-group'>
+                        <label htmlFor="password">Request speed(ms): </label>
+                        <input type="text" className="form-control" name="speed" disabled={playing} value={speed} onChange={this.handleChange} />
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor="password">Number of games: </label>
+                        <input type="text" className="form-control" name="numberG" disabled={playing} value={numberG} onChange={this.handleChange} />
+                    </div>
+
+                    <Button variant="contained" color="secondary" disabled={playing} onClick={this.handlePlay} className="btn btn-primary" style={{margin: 10}}>Play</Button>
+                    <Button variant="contained" color="secondary" onClick={this.handleReset} className="btn btn-primary" style={{margin: 10}}>Reset</Button>
+
+                    <div>Player 1 wins: {this.state.wins[0]}</div>
+                    <div>Player 2 wins: {this.state.wins[1]}</div>
                 </form>
             <hr/>
             <textarea name="body"
             disabled={true}
             value={this.state.text_field}
-            style={{width: 400, height: 400}}/>
+            style={{width: 600, height: 200, borderRadius: 5}}/>
         </div>
       );
   }
